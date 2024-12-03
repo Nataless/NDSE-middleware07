ecfec7f0d83156b4850d0d54c8235d27f40885ed
Natalie Voronova <Nataless@yandex.ru>
2024-12-04 02:35:32 +0300

    saved changes

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const index = require("./routes/index");
+const user = require("./routes/user");
+const addBooks = require("./routes/addbooks");
+const editBooks = require("./routes/editbooks");
+const delBooks = require("./routes/delbooks");
+const downloadBooks = require("./routes/download");
+const error404 = require('./middleware/err-404');
+
+const app = express();
+app.use(express.json());
+
+app.use("", index);
+app.use("", user);
+app.use("", addBooks);
+app.use("", editBooks);
+app.use("", delBooks);
+app.use("", downloadBooks);
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(error404);
+
+const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+  console.log (`Server listening port 3000.`)
+})
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const store = require("../components/store");
+
+const router = express.Router();
+
+router.get('/api/books', (req, res) => {
+    const { books } = store;
+    res.json(books);
+});
+
+router.get('/api/books/:id', (req, res) => {
+    const { books } = store;
+    const { id } = req.params;
+    const book = books.find(el => el.id === id);
+
+    if (!book) {
+        res.status(404).type('text/plain');
+        res.json('404 | страница не найдена');
+
+        return;
+    }
+
+    res.json(book);
+});
+
+module.exports = router;
